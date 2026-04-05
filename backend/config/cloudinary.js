4ad14051732143dbd8addee16b016b6d980b8e19
Prathamesh79097 <prathameshspatil1305@gmail.com>
2026-04-05 18:32:35 +0530

    update changes

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 // Links to the keys we will hide in the .env file
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_KEY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
